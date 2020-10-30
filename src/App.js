b3865edfffec2f42f01273651dd0a155aeee89a5
Ayano Taki <ayanobayern2@gmail.com>
2020-10-30 21:30:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select value='number'></select>
+      <div>
+        10月30日
+      </div>
+      <div>
+        2020
+      </div>
+      <div>
+        コーポレートサイトの配色を考えました。Gatsby.jsとcontentfulの勉強をしました。
+        CMSが実装できそうなのでコーポレートサイトにも取り入れたいと思います。
+      </div>
     </div>
   );
 }
