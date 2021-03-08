2af70794eca4141941cf9ff677d19bed92f8c707
Ayano Taki <ayanobayern2@gmail.com>
2021-03-08 23:44:17 +0900

    firebase インストール

--- src/components/PageEdit.js
+++ src/components/PageEdit.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PageEdit = () => {
+  const [diary, setDiary] = useState({
+    date: '',
+    title: '',
+    text: '',
+  })
+  
   return (
-    <div>
-      Edit
-    </div>
+    <>
+    <p>
+      日付：{diary.date}
+    </p>
+    <p>
+      タイトル：{diary.title}
+    </p>
+    <p>
+      本文：{diary.text}
+    </p>
+    <form>
+      <label>
+        日付：
+        <input type='date'/>
+      </label>
+      <label>
+        タイトル：
+        <input type='text'/>
+      </label>
+      <label>
+        本文：
+        <textarea/>
+      </label>
+      <input type='submit' value='submit'/>
+    </form>
+    </>
   )
 }
 
